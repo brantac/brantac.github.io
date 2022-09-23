e88b86ff8ddfea08ad9b39a1398b3dd130b0c863
Arthur Brant <arthurcl.br@hotmail.com>
2022-09-23 07:54:18 -0300

    create a Nav component

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <Nav />
+    <NuxtPage />
   </div>
 </template>
---
+++ components/Nav.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav
+        class="p-4 container mx-auto flex flex-row justify-between
+        md:justify-start"
+    >
+        <div>Logo</div>
+        <a href="#"
+        class="md:hidden">
+            <div>-</div>
+            <div>-</div>
+            <div>-</div>
+        </a>
+        <div 
+            class="hidden md:flex flex-row ml-4 gap-4
+            font-semibold"
+        >
+            <a href="/#portfolio">Projects</a>
+            <a href="/#blog">Blog</a>
+            <a href="/#contact">Contact</a>
+        </div>
+    </nav>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
