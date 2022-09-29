842cd908be32ee69ae6a762d1e833708826f063f
Arthur Brant <arthurcl.br@hotmail.com>
2022-09-29 14:12:52 -0300

    add transformations to menu toggle icon

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,15 +1,9 @@
 <template>
     <nav
         class="p-4 container mx-auto flex flex-row justify-between
-        md:justify-start"
+        md:justify-start items-center relative"
     >
         <div>Logo</div>
-        <a href="#"
-        class="md:hidden">
-            <div>-</div>
-            <div>-</div>
-            <div>-</div>
-        </a>
         <div 
             class="hidden md:flex flex-row ml-4 gap-4
             font-semibold"
@@ -18,11 +12,41 @@
             <a href="/#blog">Blog</a>
             <a href="/#contact">Contact</a>
         </div>
+        <!-- Mobile nav -->
+        <a href="#" id="_menutoggleicon"
+            class="md:hidden w-11 h-11 inline-block
+            relative
+            group"
+            :class="{menuopen: mobileNavOpen}"
+            @click="() => mobileNavOpen = !mobileNavOpen"
+        >
+            <span 
+                class="w-11 h-1 bg-indigo-600 rounded absolute
+                duration-200 [.menuopen_>_&]:translate-y-5
+                [.menuopen_>_&]:rotate-45"
+            ></span>
+            <!-- <div class="w-10 h-1 bg-indigo-600 absolute mt-5 rounded"></div> -->
+            <span 
+                class="w-11 h-1 bg-indigo-600 absolute mt-10 rounded
+                duration-200 [.menuopen_>_&]:-translate-y-5
+                [.menuopen_>_&]:-rotate-45"
+            ></span>
+        </a>
+        <div 
+            class="md:hidden absolute flex flex-col 
+            left-0 pl-4 gap-4 top-0 pt-12 font-semibold
+            w-3/4 bg-indigo-600 h-screen"
+        >
+            <a href="/#portfolio">Projects</a>
+            <a href="/#blog">Blog</a>
+            <a href="/#contact">Contact</a>
+        </div>
+        <!-- /Mobile nav -->
     </nav>
 </template>
 
 <script setup lang="ts">
-
+const mobileNavOpen = ref(false)
 </script>
 
 <style scoped>
