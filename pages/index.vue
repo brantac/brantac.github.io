c38a21e6485b93da5eb381904ca5e20d0bd8b893
Arthur Brant <arthurcl.br@hotmail.com>
2022-10-06 11:57:49 -0300

    style hero section and add dark mode

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,52 @@
 <template>
-    <div>
-        <h1>
-            <span class="">Hi, my name is</span>
-            <span class="">Arthur Brant.</span>
-            <span class="">I build great experiences for the internet.</span>
+    <!-- Hero section -->
+    <div class="container mx-auto px-4 pt-4 md:pt-8">
+        <h1 class="text-left sm:text-center lg:text-left">
+            <span
+                class="block
+                text-base font-bold
+                text-zinc-900 dark:text-zinc-100
+                mb-4"
+            >
+                Hi, my name is
+            </span>
+            <span
+                class="block
+                text-4xl font-bold
+                text-zinc-900 dark:text-zinc-100
+                mb-4"
+            >
+                Arthur Brant.
+            </span>
+            <span
+                class="block
+                text-4xl font-bold
+                text-zinc-400
+                mb-4"
+            >
+                I build great experiences for the internet.
+            </span>
         </h1>
+        <p
+            class="text-base text-left sm:text-center lg:text-left
+            text-zinc-900 dark:text-zinc-100
+            max-w-md sm:mx-auto lg:mx-0"
+        >
+            I'm a fullstack web developer who wants to build
+            great experiences for people who wants to showcase
+            their portfolio, art or business. Currently, I'm working
+            as a freelancer.
+        </p>
+        <!-- TODO: center button in sm and md breakpoints -->
+        <button
+            type="button"
+            class="bg-indigo-600 hover:bg-indigo-500
+            text-zinc-100 font-semibold
+            p-4 mt-12 rounded
+            block mx-0 sm:mx-auto lg:mx-0"
+        >Send a message!</button>
     </div>
+    <!-- /Hero section -->
 </template>
 
 <script setup lang="ts">
