1754f997cd3d2feb4568ed7ba4ab84443436934a
Arthur Brant <arthurcl.br@hotmail.com>
2022-10-21 14:14:43 -0300

    add padding to project header and shadow to card

--- pages/index.vue
+++ pages/index.vue
@@ -116,33 +116,34 @@
         id="#portfolio"
         class="container mx-auto px-4 py-14"
     >
-        <h2 class="text-2xl lg:text-3xl font-bold
+        <h2 class="text-2xl lg:text-3xl font-medium
         text-zinc-400
         text-left sm:text-center
-        mb-4 sm:mb-8">Projects</h2>
-        <div class="w-full flex flex-col gap-8
+        mb-4 sm:mb-12">Projects</h2>
+        <div class="w-full flex flex-col gap-12
         items-start sm:items-center">
             <div v-for="project in portfolio" :key="project.id"
             class="max-w-lg
-            rounded 
-            shadow shadow-neutral-500 p-4">
-                <h3 class="text-xl lg:text-2xl tracking-wide
-                sm:text-center
-                text-zinc-900 dark:text-zinc-100
-                font-bold
-                ">
-                    {{ project.title }}
-                </h3>
-                <div class="flex flex-row gap-4 mb-4
-                sm:justify-center">
-                    <span v-for="tech in project.tech"
-                    class="text-sm text-zinc-400">{{ tech }}</span>
+            rounded
+            bg-neutral-200/40 dark:bg-neutral-800/40
+            shadow-md shadow-neutral-700/50
+            hover:shadow-lg hover:shadow-neutral-700/50">
+                <div class="p-8">
+                    <h3 class="text-xl lg:text-2xl tracking-wide
+                    
+                    text-neutral-700 dark:text-zinc-100
+                    font-medium">
+                        {{ project.title }}
+                    </h3>
+                    <div class="flex flex-row gap-4">
+                        <span v-for="tech in project.tech"
+                        class="text-sm text-neutral-400">{{ tech }}</span>
+                    </div>
                 </div>
                 <img :src="project.thumbnail"
                 :alt="'Thumbnail for the project ' + project.title"
                 class="w-full h-auto text-zinc-900 dark:text-zinc-100
-                rounded">
-                <!-- rounded shadow-md shadow-indigo-600/50"> -->
+                rounded-b">
             </div>
         </div>
     </section>
