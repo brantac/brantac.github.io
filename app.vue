425ff63844e74863a7c24c1c89dfcf0dfe74f8ed
Arthur Brant <arthurcl.br@hotmail.com>
2022-10-26 09:11:40 -0300

    set html lan attribute to english

--- app.vue
+++ app.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
   useHead({
+    htmlAttrs: {
+      lang: 'en'
+    },
     bodyAttrs: {
       class: `bg-neutral-50 dark:bg-neutral-900
       [&:has(a.menuopen)]:bg-gray-800/50
       md:[&:has(a.menuopen)]:bg-neutral-50
       dark:md:[&:has(a.menuopen)]:bg-neutral-900`,
-    }
+    },
   })
 </script>
 
