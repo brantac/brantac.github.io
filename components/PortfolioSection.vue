5bfe30d5cb2d6ec3ca8024490627ec38b2096ea9
Arthur Brant <arthurcl.br@hotmail.com>
2024-10-17 17:29:52 -0300

    style: add padding to project card images

--- components/PortfolioSection.vue
+++ components/PortfolioSection.vue
@@ -6,29 +6,22 @@
         <h2 class="text-2xl lg:text-4xl font-medium text-zinc-900 dark:text-zinc-100 text-left sm:text-center mb-4 sm:mb-12">Portfolio</h2>
         <div class="w-full flex flex-col gap-12
         items-start sm:items-center">
-            <div v-for="project in portfolio" :key="project.id"
-            class="max-w-lg
-            rounded
-            bg-gray-50 dark:bg-neutral-800/40
-            shadow-md shadow-neutral-700/50
-            hover:shadow-lg hover:shadow-neutral-700/50">
-                <div class="p-8">
-                    <h3 class="text-xl lg:text-2xl tracking-wide
-    
-                    text-neutral-700 dark:text-zinc-100
-                    font-medium">
+            <a :href="project.link" target="_blank" v-for="project in portfolio" :key="project.id"
+            class="max-w-lg rounded-xl bg-indigo-50 dark:bg-zinc-800 shadow-md shadow-neutral-700/50 hover:shadow-lg hover:shadow-neutral-700/50">
+                <div class="px-4 pt-4">
+                    <h3 class="text-xl lg:text-2xl tracking-wide text-zinc-950 dark:text-zinc-50 font-medium">
                         {{ project.title }}
                     </h3>
                     <div class="flex flex-row gap-4">
                         <span v-for="tech in project.tech"
-                        class="text-sm text-neutral-400">{{ tech }}</span>
+                        class="text-sm font-medium text-zinc-500 dark:text-zinc-400">{{ tech }}</span>
                     </div>
                 </div>
                 <img :src="project.thumbnail"
                 :alt="'Thumbnail for the project ' + project.title"
                 class="w-full h-auto text-zinc-900 dark:text-zinc-100
-                rounded-b">
-            </div>
+                rounded-3xl p-4 pt-4">
+            </a>
         </div>
     </section>
 </template>
@@ -37,9 +30,17 @@
 const portfolio = [
     {
         id: 1,
-        title: "Personal portfolio website",
+        title: "My Portfolio Website",
         tech: ["Vue 3", "TypeScript", "Nuxt"],
-        thumbnail: "/Portfolio_3.jpg"
+        thumbnail: "/Portfolio_3.jpg",
+        link: "https://github.com/brantac/brantac.github.io"
+    },
+    {
+        id: 2,
+        title: "Customer Registration App",
+        tech: ["Vue.js 3", "Java", "Spring Boot"],
+        thumbnail: "/Thumbnail_CustomerRegistrationApp.jpg",
+        link: "https://github.com/brantac/customer-registration-app"
     },
 ]
 </script>
