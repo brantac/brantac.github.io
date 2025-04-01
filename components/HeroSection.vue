5c268757ac73e1efb12e24551f5f25300f9fb7e5
Arthur Brant <arthurcl.br@hotmail.com>
2025-04-01 18:28:27 -0300

    feat:write a more nuanced text about me in hero section

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -4,33 +4,18 @@
         <div class="hero-left-side sm:flex-1 w-full lg:max-w-[50%] min-h-[var(--hero-section-height)] flex flex-col justify-center items-center">
             <h1 class="text-center text-4xl md:text-5xl lg:text-6xl font-medium"
             >
-                <span
-                    class="block
-                    text-zinc-900 dark:text-zinc-100
-                    mb-4"
-                >
+                <span class="block text-zinc-900 dark:text-zinc-100 mb-4">
                     Welcome
                 </span>
-                <span
-                    class="block
-                    text-zinc-900 dark:text-zinc-100
-                    mb-4"
-                >
+                <span class="block text-zinc-900 dark:text-zinc-100 mb-4">
                     My name is
                 </span>
-                <span
-                    class="block
-                    text-indigo-500 dark:text-indigo-500
-                    mb-4"
-                >
+                <span class="block text-indigo-500 dark:text-indigo-500 mb-4">
                     Arthur Brant
                 </span>
             </h1>
-            <p
-                class="text-center
-                text-xl lg:text-2xl font-medium text-zinc-900 dark:text-zinc-100"
-            >
-                I build great experiences for the internet
+            <p class="text-center text-xl font-medium text-zinc-900 dark:text-zinc-100">
+                I build simple and effective applications. I am a Full-Stack Web Developer who enjoys programming in Java, Vue.js and TypeScript. I am currently looking for a job in a junior role.
             </p>
         </div>
         <div class="hero-right-side sm:flex-1 w-fit lg:max-w-[40%] rounded-xl border border-zinc-200 dark:border-zinc-700 bg-white dark:bg-zinc-800 shadow-lg p-4">
@@ -38,9 +23,9 @@
                 class="text-xl font-bold text-zinc-900 dark:text-zinc-100"
             >I am</p>
             <ul class="text-xl space-y-2 mb-4">
-                <ListItem>a Computer Scientist</ListItem>
-                <ListItem>a Fullstack web developer</ListItem>
-                <ListItem>Curious</ListItem>
+                <ListItem>A Computer Scientist</ListItem>
+                <ListItem>A full-stack web developer</ListItem>
+                <ListItem>Eager to learn new technologies</ListItem>
             </ul>
             <p class="text-xl font-bold text-zinc-900 dark:text-zinc-100">I know</p>
             <ul class="text-xl space-y-2">
@@ -49,6 +34,7 @@
                 <ListItem>Node</ListItem>
                 <ListItem>MySQL</ListItem>
                 <ListItem>MongoDB</ListItem>
+                <ListItem>And many more...</ListItem>
             </ul>
         </div>
     </section>
